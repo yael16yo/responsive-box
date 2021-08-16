ffcd7d08eec579378d926bc439c077866e82f76b
yael16yo <yael.brinkert@gmail.com>
2021-08-16 15:37:06 +0200

    sum

---
+++ index.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>yoo - votre site internet sur-mesure</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/javascript" href="js/main/js">
+	 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+
+	<header class="main-header" id="top">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg main-nav px-0">
+        <a class="navbar-brand" href="#">
+					      <img src="img/yoologov3.png" alt="yaeloo.com">
+					    </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainMenu" aria-controls="mainMenu" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="icon-bar icon-bar-1"></span>
+                        <span class="icon-bar icon-bar-2"></span>
+                        <span class="icon-bar icon-bar-3"></span>
+                    </button>
+        <div class="collapse navbar-collapse" id="mainMenu">
+          <ul class="navbar-nav ml-auto text-uppercase f1">
+            <li>
+              <a href="#top" class="active active-first">home</a>
+            </li>
+            <li>
+              <a href="#about">about us</a>
+            </li>
+            <li>
+              <a href="#service">services</a>
+            </li>
+            <li>
+              <a href="#project">projects</a>
+            </li>
+            <li>
+              <a href="#team">team</a>
+            </li>
+            <li>
+              <a href="#partners">partners</a>
+            </li>
+            <li>
+              <a href="#contact">contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    <!-- /.container -->
+  </header>
+  
+  	<section class="bg-top" id="home">
+  		<div class="home-table">
+                <div class="home-table-center" style="padding-top: 250px;padding-bottom: 250px">
+                    <div class="container position-relative z-index">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="text-white text-center">
+                                    <h1 class="header_title mx-auto headline" style="font-family: 'Montserrat';"><b>« Innovation, Creation »</b></h1>
+                                    <p class="header_subtitle mx-auto pt-4 mb-0 pb-2 text-white tagline" style="font-family: 'Montserrat';">Un travail attentif et délicat pour un résultat à vos attentes. <br>
+                                    	C'est ce que les mots <b>« Innovation »</b> et <b>« Creation »</b> veulent dire.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+  	</section>
+  	<section class="aboutus" id="about">
+  		<div class="canvas-wrapper">
+	<a href="https://discord.gg/WE8vTT69YN" target="_blank" class="canvas">
+		<div class="canvas_border">
+			<svg>
+				<defs><linearGradient id="grad-orange" x1="0%" y1="0%" x2="100%" y2="0%"><stop offset="0%" style="stop-color:rgb(253,137,68);stop-opacity:1"></stop><stop offset="100%" style="stop-color:rgb(153,75,23);stop-opacity:1"></stop></linearGradient><linearGradient id="grad-red" x1="0%" y1="0%" x2="100%" y2="0%"><stop offset="0%" stop-color="#D34F48"></stop><stop offset="100%" stop-color="#772522"></stop></linearGradient></defs>
+				<rect id="rect-grad" class="rect-gradient" fill="none" stroke="url(#grad-orange)" stroke-linecap="square" stroke-width="4" stroke-miterlimit="30" width="100%" height="100%"></rect>
+			</svg>
+		</div>
+		<div class="canvas_img-wrapper">
+			<img class="canvas_img" src="https://www.freeiconspng.com/uploads/discord-black-icon-1.png" alt="">
+		</div>
+		<div class="canvas_copy canvas_copy--left">
+			<span class="canvas_copy_subtitle">Discord</span>
+			<strong class="canvas_copy_title">Join</strong>
+			<strong class="canvas_copy_title">Me</strong>
+			<span class="canvas_copy_details">yaelou#5185</span>
+		</div>
+	</a>
+	<a href="https://twitter.com/yael_brkt" target="_blank" class="canvas">
+		<div class="canvas_border">
+			<svg>
+				<defs><linearGradient id="grad-orange" x1="0%" y1="0%" x2="100%" y2="0%"><stop offset="0%" style="stop-color:rgb(253,137,68);stop-opacity:1"></stop><stop offset="100%" style="stop-color:rgb(153,75,23);stop-opacity:1"></stop></linearGradient><linearGradient id="grad-red" x1="0%" y1="0%" x2="100%" y2="0%"><stop offset="0%" stop-color="#D34F48"></stop><stop offset="100%" stop-color="#772522"></stop></linearGradient></defs>
+				<rect id="rect-grad" class="rect-gradient" fill="none" stroke="url(#grad-orange)" stroke-linecap="square" stroke-width="4" stroke-miterlimit="30" width="100%" height="100%"></rect>
+			</svg>
+		</div>
+		<div class="canvas_img-wrapper">
+			<img class="canvas_img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/249772/Twitter_Logo_Blue.png" alt="">
+		</div>
+		<div class="canvas_copy">
+			<span class="canvas_copy_subtitle">Twitter</span>
+			<strong class="canvas_copy_title">Join</strong>
+			<strong class="canvas_copy_title">Me</strong>
+			<span class="canvas_copy_details">@yael_brkt</span>
+		</div>
+	</a>
+</div>
+  	</section>
+
+
+
+  <script src="https://unpkg.com/scrollreveal"></script>
+  <script type="text/javascript">
+  	ScrollReveal({ reset: true });
+  	(function scrollReveal() {
+  window.sr = ScrollReveal();
+  
+  sr.reveal('.headline', {
+    duration   : 600,
+    distance   : '20px',
+    easing     : 'ease-out',
+    origin     : 'bottom',
+    reset      : true,
+    scale      : 1,
+    viewFactor : 0,
+    afterReveal  : revealChildren,
+  }, 150);
+  
+    var revealChildren = sr.reveal('.tagline', {
+    duration   : 600,
+    scale      : 1,
+    distance   : '20px',
+    origin     : 'bottom',
+    reset      : true,
+    easing     : 'ease-out',
+    viewFactor : 1,
+  }, 75);
+})();
+
+	(function scrollReveal() {
+  window.sr = ScrollReveal();
+  
+  sr.reveal('.canvas-wrapper', {
+    duration   : 1300,
+    distance   : '20px',
+    easing     : 'ease-out',
+    origin     : 'bottom',
+    reset      : true,
+    scale      : 1,
+    viewFactor : 0,
+    afterReveal  : revealChildren,
+  }, 150);
+  
+    var revealChildren = sr.reveal('.card-title, .card-text', {
+    duration   : 500,
+    scale      : 1,
+    distance   : '20px',
+    origin     : 'bottom',
+    reset      : true,
+    easing     : 'ease-out',
+    viewFactor : 1,
+  }, 75);
+})();
+
+  </script>
+  <script src="js/particles.js"></script>
+  <script src="js/particles.app.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
